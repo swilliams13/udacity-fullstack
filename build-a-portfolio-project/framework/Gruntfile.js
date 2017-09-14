b04eef451d6f1b87e96fbb87d6a2b51c8829c99b
Samantha Williams <swilliams13@gmail.com>
2017-09-14 15:16:38 -0400

    Add Portfolio Project
    Udacity Build A Portfolio Site Project - Submission 1

---
+++ build-a-portfolio-project/framework/Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 800,
+            suffix: '_large_1x',
+            quality: 50
+          },
+          {
+            width: 600,
+            suffix: '_medium',
+            quality: 50
+          },
+          {
+            width: 425,
+            suffix: '_small',
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img_src/',
+          dest: 'img/'
+        }]
+      }
+    },
+     /* Clear out the images directory if it exists */
+    clean: {
+      dev: {
+        src: ['img'],
+      },
+    },
+
+    /* Generate the images directory if it is missing */
+    mkdir: {
+      dev: {
+        options: {
+          create: ['img']
+        },
+      },
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.registerTask('default', ['clean', 'mkdir', 'responsive_images']);
+
+};
