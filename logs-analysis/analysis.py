613cf38bf8e287489af8f077ff4780f2e852f35a
Samantha Williams <swilliams13@gmail.com>
2017-09-20 21:56:15 -0400

    Pep8 format

--- logs-analysis/analysis.py
+++ logs-analysis/analysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import psycopg2
 
+
 def connect(database_name="news"):
     try:
         db = psycopg2.connect("dbname={}".format(database_name))
@@ -9,12 +10,14 @@ def connect(database_name="news"):
     except:
         print("<error message>")
 
+
 def runQuery(cursor, query):
     try:
         cursor.execute(query)
         return cursor.fetchall()
     except:
         print("<error message>")
+
 
 def logAnalysis():
     db, cursor = connect()
@@ -25,20 +28,21 @@ def logAnalysis():
               "where log_views.path like concat('%', articles.slug) "
               "order by views desc limit 3;")
 
-    query2 = ("select name, sum(views) as totalViews from articles, authors, "
-              "(select count(*) as views, path from log group by path) as "
-              "log_views where articles.author = authors.id and log_views.path "
-              "like concat('%', articles.slug) group by name order by "
-              "totalViews desc;")
+    query2 = ("select name, sum(views) as totalViews from articles, authors,"
+              " (select count(*) as views, path from log group by path) as "
+              "log_views where articles.author = authors.id and "
+              "log_views.path like concat('%', articles.slug) group by name"
+              " order by totalViews desc;")
 
     query3 = ("select round((num_errors/num_total)*100,1) as percentage, "
               "to_char(total.date, 'FMMonth DD, YYYY') as date from "
               "(select cast(count(id) as decimal) as num_errors, "
               "date_trunc('day',time) as date from log where "
-              "(status LIKE '4%' OR status LIKE '5%') group by date) as errors, "
+              "(status LIKE '4%' OR status LIKE '5%') group by date) "
+              "as errors, "
               "(select cast(count(id) as decimal) as num_total, "
-              "date_trunc('day',time) as date from log group by date) as total "
-              "where total.date = errors.date and "
+              "date_trunc('day',time) as date from log group by date) "
+              "as total where total.date = errors.date and "
               "((num_errors/num_total)*100) > 1.0;")
 
     results = runQuery(cursor, query1)
